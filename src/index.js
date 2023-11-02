e5ac46a595060020a5aab22bc1c1c215d2db1b62
Alexandr Kudryavtcev <k.v.alexandr@gmail.com>
2023-11-02 22:05:28 +0400

    add: argument and option

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import { program } from 'commander';
 
 const gendiff = () => {
-  program.description('Compares two configuration files and shows a difference.').version('1.0.0');
+  program
+    .description('Compares two configuration files and shows a difference.')
+    .version('1.0.0')
+    .option('-f, --format <type>', 'output format')
+    .argument('<filepath1> <filepath2>');
 
   program.parse();
 };
